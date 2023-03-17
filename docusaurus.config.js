2e08d33814269d589ab22acacff3c93ddf20d2bf
Abheek Dhawan <abheekd@protonmail.com>
2023-03-17 13:59:20 -0500

    fix: update name to match new repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://rambunction4330.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/RambunctionDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rambunction4330', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'RambunctionDocs', // Usually your repo name.
   trailingSlash: false,
 
   // onBrokenLinks: 'throw',
