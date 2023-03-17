5687834678c5b270f4b1c81b28249250468d94cd
Abheek Dhawan <abheekd@protonmail.com>
2023-03-17 15:31:05 -0500

    feat: update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'RambunctionDocs',
+  title: 'Rambunction Docs',
   tagline: 'what does a boolean mean',
   favicon: 'img/favicon.ico',
 
