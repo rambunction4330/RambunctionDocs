9956355568f8182ef4998d93365ff31a8e617ec0
Abheek Dhawan <abheekd@protonmail.com>
2023-03-17 13:45:48 -0500

    fix: update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,23 +98,23 @@ const config = {
               },
             ],
           },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
+//           {
+//             title: 'Community',
+//             items: [
+//               {
+//                 label: 'Stack Overflow',
+//                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+//               },
+//               {
+//                 label: 'Discord',
+//                 href: 'https://discordapp.com/invite/docusaurus',
+//               },
+//               {
+//                 label: 'Twitter',
+//                 href: 'https://twitter.com/docusaurus',
+//               },
+//             ],
+//           },
           {
             title: 'More',
             items: [
@@ -124,12 +124,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Rambunction4330',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Rambunction4330. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
