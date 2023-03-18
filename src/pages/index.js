242430d405145f568b826ee0f13ad4268bb3b732
Abheek Dhawan <abheekd@protonmail.com>
2023-03-17 21:21:37 -0500

    feat: add some personality to home page (remove later!!!!!)

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        {/*<p className="hero__subtitle">{siteConfig.tagline}</p>*/}
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -34,6 +34,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+        <img style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '2rem', marginBottom: '2rem'}} src='https://camo.githubusercontent.com/81a1ca0b47997bb238f03f90045396d431a0a4530e4b7bc460ade71cacd49919/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3930333234333535333134333134343434382f3930333730393636393735333737303030342f63617074696f6e2e676966' />
         {/*<HomepageFeatures />*/}
       </main>
     </Layout>
